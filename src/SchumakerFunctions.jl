1ef2c19843d0a0871b0a1c7dd5318626dceff4c2
Stuart Baumann <stuart@stuartbaumann.com>
2019-02-17 12:51:39 +0000

    Bugfix - Potentially left_root will actually be the right_root and vice versa. So need parallel logic.

--- src/SchumakerFunctions.jl
+++ src/SchumakerFunctions.jl
@@ -204,20 +204,20 @@ function find_roots(spline::Schumaker)
                 a = spline.coefficient_matrix_[i,1]
                 b = spline.coefficient_matrix_[i,2]
                 c = spline.coefficient_matrix_[i,3]
-                if abs(a) > 1e-13
+                if abs(a) > 1e-13 # Is it quadratic
                     det = sqrt(b^2 - 4*a*c)
                     left_root  = (-b + det) / (2*a) # The x coordinate here is relative to spline.IntStarts_[i]. We want the smallest one that is to the right (ie positive)
                     right_root = (-b - det) / (2*a)
-                    if left_root > 1e-13
+                    if (left_root > 1e-15) && (left_root < spline.IntStarts_[i+1] - spline.IntStarts_[i] +  1e-15)
                         append!(roots, left_root + spline.IntStarts_[i])
                         append!(first_derivatives, 2 * a * left_root + b)
                         append!(second_derivatives, 2 * a)
-                    elseif right_root < spline.IntStarts_[i+1] - spline.IntStarts_[i] +  1e-13
+                    elseif (right_root > 1e-15) && (right_root < spline.IntStarts_[i+1] - spline.IntStarts_[i] +  1e-15)
                         append!(roots, right_root + spline.IntStarts_[i])
                         append!(first_derivatives, 2 * a * right_root + b)
                         append!(second_derivatives, 2 * a)
                     end
-                else # My be linear. Cannot be constant or else it could not have jumped past zero.
+                else # Is it linear? Note it cannot be constant or else it could not have jumped past zero in the interval.
                     new_root = spline.IntStarts_[i] - c/b
                     if !((length(roots) > 0) && (abs(new_root - last(roots)) < 1e-5))
                         append!(roots, spline.IntStarts_[i] - c/b)
