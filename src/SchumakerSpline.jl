e13fcd5a855d874edaefbc82828f05c0b416df34
Stuart Baumann <stuart@stuartbaumann.com>
2019-06-15 23:41:18 +0100

    Adding ability to splice splines.

--- src/SchumakerSpline.jl
+++ src/SchumakerSpline.jl
@@ -8,4 +8,6 @@ export Schumaker_ExtrapolationSchemes, Curve, Linear, Constant
 export Schumaker, evaluate, evaluate_integral
 include("roots_optima_intercepts.jl")
 export find_derivative_spline, find_roots, find_optima, get_intersection_points
+include("splice_splines.jl")
+export splice_splines
 end
